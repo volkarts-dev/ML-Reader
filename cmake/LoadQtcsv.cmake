fc64be27f8ac4d1df0d0816ebff1357a95520f94
Daniel Volk <mail@volkarts.com>
2023-10-22 19:45:26 +0200

    Honor CMP0077

--- cmake/LoadQtcsv.cmake
+++ cmake/LoadQtcsv.cmake
@@ -1,6 +1,6 @@
-set(STATIC_LIB ON)
-set(USE_QT4 OFF)
-set(BUILD_TESTS OFF)
+set(STATIC_LIB ON CACHE BOOL "Build qtcsv static lib" FORCE)
+set(USE_QT4 OFF CACHE BOOL "Don't build qtcsv against qt4" FORCE)
+set(BUILD_TESTS OFF CACHE BOOL "Don't build qtcsv tests" FORCE)
 add_subdirectory(third_party/qtcsv)
 
 add_library(qtcsv::qtcsv ALIAS qtcsv)
