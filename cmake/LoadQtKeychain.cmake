3b00c2a957714442940f3498d82cee7464c394c7
Daniel Volk <mail@volkarts.com>
2024-01-12 15:21:29 +0100

    Fix compilation with local qtkeychain

--- cmake/LoadQtKeychain.cmake
+++ cmake/LoadQtKeychain.cmake
@@ -2,4 +2,9 @@ set(BUILD_WITH_QT5 OFF CACHE BOOL "" FORCE)
 set(BUILD_SHARED_LIBS OFF CACHE BOOL "" FORCE)
 add_subdirectory(third_party/qtkeychain)
 
-add_library(qtkeychain::qt5keychain ALIAS qt5keychain)
+add_library(qt5keychain-iface INTERFACE)
+add_library(qtkeychain::qt5keychain ALIAS qt5keychain-iface)
+
+target_include_directories(qt5keychain-iface SYSTEM INTERFACE third_party/qtkeychain)
+target_compile_definitions(qt5keychain-iface INTERFACE QTKEYCHAIN_NO_EXPORT)
+target_link_libraries(qt5keychain-iface INTERFACE qt5keychain)
