445fee10e92a47f8545ad51725846f05362bf680
Daniel Volk <mail@volkarts.com>
2024-01-12 12:57:22 +0100

    Add qtkeychain as submodule

--- cmake/LoadQtKeychain.cmake
+++ cmake/LoadQtKeychain.cmake
@@ -1,1 +1,5 @@
-find_package(Qt5Keychain REQUIRED)
+set(BUILD_WITH_QT5 OFF CACHE BOOL "" FORCE)
+set(BUILD_SHARED_LIBS OFF CACHE BOOL "" FORCE)
+add_subdirectory(third_party/qtkeychain)
+
+add_library(qtkeychain::qt5keychain ALIAS qt5keychain)
