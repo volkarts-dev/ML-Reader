5b6e92455dbaf3b2d25337426f9c51ffec47eddc
Daniel Volk <mail@volkarts.com>
2024-02-19 11:12:08 +0100

    Fix git version reader

--- cmake/GitVersion.cmake
+++ cmake/GitVersion.cmake
@@ -1,6 +1,3 @@
-
-set(GIT_VERSION_TARGET_DIR ${CMAKE_BINARY_DIR}/git_version)
-
 function(get_git_version config_file_in config_file_out)
     set(CACHE_FILE ${GIT_VERSION_TARGET_DIR}/git_state.txt)
 
@@ -28,28 +25,32 @@ function(get_git_version config_file_in config_file_out)
 
     if(NOT ${GIT_HASH} STREQUAL ${GIT_HASH_CACHE} OR NOT EXISTS ${config_file_out})
         file(WRITE ${CACHE_FILE} "${GIT_HASH}\n${GIT_TIME}")
+        file(REMOVE ${config_file_out})
         configure_file(${config_file_in} ${config_file_out} @ONLY)
     endif()
 endfunction()
 
 function(setup_git_version_config target config_file_in)
+    set(GIT_VERSION_TARGET_DIR ${CMAKE_BINARY_DIR}/git_version)
+
+    get_filename_component(abs_config_file_in ${config_file_in} ABSOLUTE)
     get_filename_component(config_file_name ${config_file_in} NAME_WLE)
     set(config_file_out ${GIT_VERSION_TARGET_DIR}/${config_file_name})
 
     add_custom_target(CheckGitVersion COMMAND ${CMAKE_COMMAND}
-        -DRUN_CHECK_GIT_VERSION=1
-        -DGIT_VERSION_CONFIG_FILE_IN=${config_file_in}
+        -DRUN_GIT_VERSION_CHECK=1
+        -DGIT_VERSION_TARGET_DIR=${GIT_VERSION_TARGET_DIR}
+        -DGIT_VERSION_CONFIG_FILE_IN=${abs_config_file_in}
         -DGIT_VERSION_CONFIG_FILE_OUT=${config_file_out}
-        -DGIT_HASH_CACHE=${GIT_HASH_CACHE}
-        -DGIT_TIME_CACHE=${GIT_TIME_CACHE}
         -P ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/GitVersion.cmake
         BYPRODUCTS ${config_file_out}
     )
 
     add_dependencies(${target} CheckGitVersion)
-    target_include_directories(${target} INTERFACE ${GIT_VERSION_TARGET_DIR})
+    target_sources(${target} PRIVATE ${config_file_out})
+    target_include_directories(${target} PRIVATE ${GIT_VERSION_TARGET_DIR})
 
-    get_git_version(${config_file_in} ${config_file_out})
+    get_git_version(${abs_config_file_in} ${config_file_out})
 endfunction()
 
 if(RUN_GIT_VERSION_CHECK AND GIT_VERSION_CONFIG_FILE_IN AND GIT_VERSION_CONFIG_FILE_OUT)
