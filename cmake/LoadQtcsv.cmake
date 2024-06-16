cdf430fb643ca570572c336fe0a344b896344272
Daniel Volk <mail@volkarts.com>
2024-06-16 21:39:52 +0200

    Use own version of qtcsv

--- cmake/LoadQtcsv.cmake
+++ cmake/LoadQtcsv.cmake
@@ -1,6 +1,5 @@
-set(STATIC_LIB ON CACHE BOOL "Build qtcsv static lib" FORCE)
-set(USE_QT4 OFF CACHE BOOL "Don't build qtcsv against qt4" FORCE)
-set(BUILD_TESTS OFF CACHE BOOL "Don't build qtcsv tests" FORCE)
+set(QTCSV_STATIC_LIB ON CACHE BOOL "Build qtcsv static lib" FORCE)
+set(QTCSV_BUILD_TESTS OFF CACHE BOOL "Don't build qtcsv tests" FORCE)
 add_subdirectory(third_party/qtcsv)
 
 add_library(qtcsv::qtcsv ALIAS qtcsv)
